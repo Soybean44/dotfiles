5512a6be81871692e0ede13c23ecbd0338895199
Sovereign Shahid <sovereignshahid@gmail.com>
2024-06-01 14:53:52 -0600

    added xmonad to my dotfiles

---
+++ .config/xmonad/barL.hs
@@ -0,0 +1,32 @@
+Config { 
+   -- appearance
+    font =         "ComicShannsMono Nerd Font 14"
+   , additionalFonts = []
+   , bgColor =      "#1e1e2e"
+   , fgColor =      "#cdd6f4"
+   , position =     Static { xpos = 0, ypos = 0, width=1920, height=26 }
+   , border =       NoBorder
+   , borderColor =  "#74c7ec"
+   , alpha = 255
+   , textOffset = -1
+   , iconOffset = -1
+   , lowerOnStart = True
+   , pickBroadest = False
+   , persistent = False
+   , hideOnStart = False
+   , allDesktops = True
+   , overrideRedirect = True
+   , commands = [ Run UnsafeXPropertyLog "_XMONAD_LOG_0"
+                , Run Weather "EGPF" ["-t", "<station>: <tempC>C",
+                                      "-L", "18", "-H", "25",
+                                      "--normal", "green",
+                                      "--high", "red",
+                                      "--low", "lightblue"] 36000
+                , Run Cpu ["-L", "3", "-H", "50",
+                           "--normal", "green", "--high", "red"] 10
+                , Run Date "%I:%M%p" "date" 10
+                ]
+   , sepChar = "%"
+   , alignSep = "}{"
+   , template = "%_XMONAD_LOG_0% }{%cpu% | %EGPF% | %date% "
+   }
