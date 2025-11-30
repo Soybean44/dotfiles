0e35804df0c723f8e15701d579f3dc979f070557
Sovereign Shahid <sovereignshahid@gmail.com>
2025-11-30 15:21:44 -0700

    updated startup script to be cross platform

---
+++ scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+nitrogen --restore &
+kanata --cfg=/home/soybean44/.config/kanata/keyboard.kbd &
+blueman-applet &
+xautolock -time 10 -locker slock &
+xsettingsd &
+dunst &
+picom --backend egl -b &
+nm-applet & 
+flameshot &
+muezzin &
+redshift -l 53.440648:-113.624623 -t 7000K:3000K -b1.0:0.6 -r&
+wmname LG3D &
