fbb9c9841e641268ecc1669da29f0eaf23356d1e
Sovereign Shahid <sovereignshahid@gmail.com>
2026-02-20 11:29:23 -0700

    removed redshift from startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,7 +7,8 @@ xsettingsd &
 dunst &
 picom --backend egl -b &
 nm-applet & 
-redshift -l 53.440648:-113.624623 -t 7000K:3000K -b1.0:0.6 -r&
+#redshift -l 53.440648:-113.624623 -t 7000K:3000K -b1.0:0.6 -r&
 wmname LG3D &
 udiskie -a &
 mpDris2 --music-dir /home/soybean44/Music/ & # yay -S mpdris2
+flameshot &
